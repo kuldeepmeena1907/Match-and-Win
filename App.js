79e518acf815d96eaba89c775c565dc722fe210f
kuldeepmeena1907 <111601655+kuldeepmeena1907@users.noreply.github.com>
2023-09-03 19:46:03 +0530

    Update App.js

--- App.js
+++ App.js
@@ -66,7 +66,7 @@ import Confetti from "react-confetti"
     return (
         <main className="main">
             {tenzies && <Confetti />}
-            <h1 className="title">Tenzies</h1>
+            <h1 className="title">Match and Win</h1>
             <p className="instructions">Roll until all dice are the same. 
             Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
